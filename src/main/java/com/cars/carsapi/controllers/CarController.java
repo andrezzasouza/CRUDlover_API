afed76a4ee2e3eda04ed3d83ec6588d0b07698cf
Andrezza Souza <andrezzasouza@id.uff.br>
2023-04-10 02:43:34 -0300

    feat: add function to alter an entry

--- src/main/java/com/cars/carsapi/controllers/CarController.java
+++ src/main/java/com/cars/carsapi/controllers/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,18 @@ public class CarController {
     System.out.println("O carro " + req.model() + " foi fabricado em " + req.manufactureDate() + " e custa R$ " + req.price() + ",00");
     System.out.println("Cadastrado com sucesso!");
     repository.save(new Car(req));
+  }
+
+  @PutMapping("/{id}")
+  public void updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO req) {
+    repository.findById(id).map(car -> {
+      car.setModel(req.model());
+      car.setManufacturer(req.manufacturer().getDescription());
+      car.setManufactureDate(req.manufactureDate());
+      car.setPrice(req.price());
+      car.setYearModel(req.yearModel());
+      return repository.save(car);
+    });
   }
 
   @DeleteMapping("/{id}")
