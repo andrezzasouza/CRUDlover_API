488bd2ad29ed3745cdba9e214b799792ebd212e3
Andrezza Souza <andrezzasouza@id.uff.br>
2023-04-10 02:32:01 -0300

    feat: add function to delete an entry

--- src/main/java/com/cars/carsapi/controllers/CarController.java
+++ src/main/java/com/cars/carsapi/controllers/CarController.java
@@ -3,7 +3,9 @@ package com.cars.carsapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,10 @@ public class CarController {
     System.out.println("O carro " + req.model() + " foi fabricado em " + req.manufactureDate() + " e custa R$ " + req.price() + ",00");
     System.out.println("Cadastrado com sucesso!");
     repository.save(new Car(req));
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteCar(@PathVariable Long id) {
+    repository.deleteById(id);
   }
 }
